addf7e8e5a3ca65d4f9432d468cf797be08f1003
Ludwig Cornely <lco@snoozing-pangolin.space>
2021-05-03 15:36:16 +0200

    updated function structure

--- 5109.py
+++ 5109.py
@@ -43,9 +43,32 @@ enc_str = "ABCDEFGHIJKLLAHMFNKOPCQQRSKTTAULHVJKLLAHM"
 
 alphabet_list = list(string.ascii_lowercase + string.digits)
 
+enc_alph_list = list("ABCDEFGHIJKLMNOPQRSTUV")
+
 enc_str_list=[]
 enc_str_list[:0]=enc_str
 
-dec_map = [36][22] #map one value from alpahbet_list to enc_str_list
+dec_map = [alphabet_list][enc_alph_list] #map one value from alpahbet_list to enc_alph_list
+#doesnt work like that, how to create clean mapping?
 
+dec_map = [len(alphabet_list)] #ref to enc_alph_list? permutate() needs logic to make sure no perms are ignored or duplicated
 
+def output_decrypt(dec_str_list, attempt_no){
+        print("Attempt: " + attempt_no + ": " + dec_str_list + "\n")
+        }
+
+def permutate(dec_map, iteration){
+        for i in #idk do
+            #do recursive logic
+        dec_str_list = decrypt(dec_map, enc_str_list)
+        output_print(dec_str_list, iteration)
+        iteration = iteration + 1
+        }
+
+def decrypt(dec_map, enc_str_list){
+        return dec_str_list
+        }
+
+def main(){
+        permutate(dec_map, 1)
+        }
