e9a0c62103cef57191c566b86307f98349d63f6c
lco-sp <lco@snoozing-pangolin.space>
2021-05-23 20:42:46 +0200

    Bugfixes and resulting data

--- list_limiter.py
+++ list_limiter.py
@@ -1,3 +1,27 @@
+def pf(word):
+    #returns true only of word matches jkllahm char distribution
+    i = 0
+    for c in word:
+        if not switcher(i, word):
+            return False
+        else:
+            i = i+1
+    if i == 7:
+        return True
+
+
+def switcher(iteration, word):
+    if iteration == 2:
+        if word[iteration] is word[3]:
+            return True 
+    elif iteration == 3:
+        if word[iteration] is word[2]:
+                return True
+    elif word.count(word[iteration]) == 1:
+        return True
+    else:
+        return False
+
 inp = open('7lwords.txt', 'r')
 outp = open('match_words.txt', 'w')
 
@@ -6,32 +30,9 @@ while True:
     if not line:
         break
     ll=list(line.strip())
-    if patternfinder(ll):
+    if pf(ll):
         outp.write(line)
-        outp.wirte("\n")
+        #outp.write("\n")
 inp.close()
 outp.close()
 
-def patternfinder(word)
-    #returns true only of word matches jkllahm char distribution
-    i = 0
-    for c in word:
-        if not switcher(i, word):
-            return False
-        else
-            i = i+1
-    if i = 7:
-        return True
-
-
-def switcher(iteration, word):
-    if iteration is 2:
-        if word[iteration] is word[3]:
-            return True 
-    elif iteration is 3:
-        if word[iteration] is word[2]:
-                return True
-    elif word.count(word[iteration]) is 1:
-        return True
-    else:
-        return False
